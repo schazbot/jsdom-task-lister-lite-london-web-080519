dda5172e98ebea5a712c423c1b79a369a332c7e9
schazbot <charlotte.pearce1984@gmail.com>
2019-10-08 10:58:55 +0100

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  const taskUl = document.querySelector("#tasks")
+
+  const form = document.querySelector("#create-task-form")
+  form.addEventListener('submit', handleFormSubmit)
+
+
+  function appendTask(text) {
+    let newLi = document.createElement('li')
+    newLi.innerText = text
+    let deleteBtn = document.createElement('button')
+    deleteBtn.innerText = "X"
+    deleteBtn.addEventListener('click', handleRemoveLi)
+    newLi.append(deleteBtn)
+    taskUl.append(newLi)
+  }
+
+  function handleFormSubmit(e) {
+    e.preventDefault()
+    text = e.target[0].value
+    appendTask(text)
+    form.reset()
+  }
+
+  function handleRemoveLi(e) {
+    liToRemove = e.target.parentElement
+    liToRemove.remove()
+  }
+
+
 });
